6003b2360fe55d43a0c272f4db49dbca04a68aca
michaelpayton124 <126622291+michaelpayton124@users.noreply.github.com>
2023-02-28 18:24:46 -0500

    Update 14.java

--- 14.java
+++ 14.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    for (int i = 1; i < 100; i++ ) {
     System.out.println("Enter input");
     String input = scanner.nextLine();
     System.out.println(Pattern.matches("(b*ab*ab*)*|(a*ba*ba*)*(c|d)*|(cbadcbad)*", input));
-  }
   }
 }
